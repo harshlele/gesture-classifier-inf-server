1ca5fd82e141614f394efe9b4c18f8af183b9fe8
Harshal lele <harshallele97@gmail.com>
2023-12-13 16:37:20 -0600

    added comments, new model

--- server.py
+++ server.py
@@ -6,31 +6,31 @@ import pathlib
 from klein import run, route
 import json
 
+#resize array to (300,9),interpolate missing values
 def resize(data):
     new_idx = np.linspace(0,data.index[-1],301)
     stretchedData = data.reindex(new_idx,method='ffill', limit=1).iloc[1:].interpolate()
     return stretchedData
 
+#this is there because there was a get_x_ts method in the notebook used for training, so fastai expects it to be there when using an exported model too..
 def get_x_ts(x):
   val = resize(pd.DataFrame(x)).values
   #val = (val - val.min(0))/(val.max(0) - val.min(0))
   return val
 
+#temporarily change PosixPath to load the model
 temp = pathlib.PosixPath
 pathlib.PosixPath = pathlib.WindowsPath
 model = load_learner("model.pkl")
 pathlib.PosixPath = temp
 
-pos_arr = np.empty((300,9))
-sz = 0
-inf_time = time.time()
-
+#predict using the model
 def detect_movement(data):
     res = model.predict(get_x_ts(data))
     print(res)
     return res
 
-
+#endpoint
 @route('/',methods=["POST"])
 def home(request):
     content = json.loads(request.content.read())    
